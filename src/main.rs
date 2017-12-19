5f47331bb32cd6fdcff52b7fc04943c63bf0a184
Jacob Helwig <jacob@technosorcery.net>
2017-12-19 08:16:55 -0800

    Support dynamically determining which column to draw the metric to

--- src/main.rs
+++ src/main.rs
@@ -18,6 +18,31 @@ use unicorn_hat_hd::{UnicornHatHd, Rotate};
 
 mod settings;
 
+enum DisplayType {
+    ColumnRatio {
+        width: u8,
+        values: Vec<u32>,
+        colors: Vec<RGB>,
+    },
+    ColumnCount {
+        width: u8,
+        value: u32,
+    },
+}
+
+#[derive(Copy, Clone)]
+struct RGB {
+    r: u8,
+    g: u8,
+    b: u8,
+}
+
+impl RGB {
+    pub fn new(red: u8, green: u8, blue: u8) -> Self {
+        Self { r: red, g: green, b: blue }
+    }
+}
+
 fn main() {
     let mut uhd = setup_unicorn_hat_hd();
 
@@ -34,13 +59,17 @@ fn main() {
         &core.handle(),
     );
 
+    let mut metrics = vec![];
+
     for repo in settings.repositories {
+        let hubcap_repo = github.repo(repo.user.clone(), repo.name.clone());
         let mut open_issues = 0;
         let mut closed_issues = 0;
+        let mut merged_issues = 0;
         let mut assigned_open_issues = 0;
 
         // Gather information about the open issues.
-        for issue in core.run(github.repo(repo.user.clone(), repo.name.clone()).issues().list(&{
+        for issue in core.run(hubcap_repo.issues().list(&{
             let mut ilo = IssueListOptionsBuilder::new();
             ilo.state(issues::State::Open);
             if let Some(ref l) = repo.labels {
@@ -51,19 +80,16 @@ fn main() {
         })).unwrap() {
             open_issues += 1;
 
-            match issue.assignee {
-                Some(_) => {
-                    if issue.closed_at == None {
-                        assigned_open_issues += 1
-                    }
-                },
-                None => {},
+            if let Some(_) = issue.assignee {
+                if issue.closed_at == None {
+                    assigned_open_issues += 1;
+                }
             };
         }
 
         // Gather information about the closed issues that were updated more
         // recently than repo.since.
-        for issue in core.run(github.repo(repo.user.clone(), repo.name.clone()).issues().list(&{
+        for issue in core.run(hubcap_repo.issues().list(&{
             let mut ilo = IssueListOptionsBuilder::new();
             ilo.state(issues::State::Closed);
             if let Some(ref l) = repo.labels {
@@ -76,39 +102,49 @@ fn main() {
             ilo.build()
         })).unwrap() {
             closed_issues += 1;
+
+            let pr = core.run(hubcap_repo.pulls().get(issue.number).get()).unwrap();
+            if let Some(_) = pr.merged_at {
+                merged_issues += 1;
+            }
         }
 
         println!("Summary ({} issues):", open_issues + closed_issues);
         println!("\tOpen: {}", open_issues);
         println!("\tClosed: {}", closed_issues);
+        println!("\tMerged: {}", merged_issues);
         println!("\tAssigned: {}", assigned_open_issues);
 
-        fill_column_ratio(&mut uhd, 0, vec![open_issues, closed_issues], vec![(0, 255, 0), (0, 0, 255)]);
-        uhd.display();
+        metrics.push(DisplayType::ColumnRatio {
+            width: 1,
+            values: vec![open_issues, closed_issues - merged_issues, merged_issues],
+            colors: vec![RGB::new(0, 255, 0), RGB::new(0, 0, 255), RGB::new(191, 119, 246)]
+        });
     }
+
+    display_metrics(&mut uhd, metrics);
 }
 
 fn setup_unicorn_hat_hd() -> UnicornHatHd {
     let mut uhd = UnicornHatHd::default();
     uhd.set_rotation(Rotate::Rot180);
-    uhd.display().unwrap();
 
     uhd
 }
 
-fn fill_column(mut uhd: &mut UnicornHatHd, col: usize, colors: Vec<(u8, u8, u8)>) -> Result<(), Error> {
+fn fill_column(uhd: &mut UnicornHatHd, col: usize, colors: Vec<RGB>) -> Result<(), Error> {
     if colors.len() > 16 {
         return Err(format_err!("Number of values ({}) cannot exceed 16.", colors.len()));
     }
 
-    for (i, &(r, g, b)) in colors.iter().enumerate() {
-        uhd.set_pixel(col, 15 - i, r, g, b);
+    for (i, &c) in colors.iter().enumerate() {
+        uhd.set_pixel(col, 15 - i, c.r, c.g, c.b);
     }
 
     Ok(())
 }
 
-fn fill_column_ratio(mut uhd: &mut UnicornHatHd, col: usize, vals: Vec<u32>, colors: Vec<(u8, u8, u8)>) -> Result<(), Error> {
+fn fill_column_ratio(mut uhd: &mut UnicornHatHd, col: usize, vals: Vec<u32>, colors: Vec<RGB>) -> Result<(), Error> {
     if vals.len() != colors.len() {
         return Err(format_err!("Number of values ({}) does not match number of colors ({}).", vals.len(), colors.len()));
     }
@@ -118,9 +154,26 @@ fn fill_column_ratio(mut uhd: &mut UnicornHatHd, col: usize, vals: Vec<u32>, col
     for (i, &v) in vals.iter().enumerate() {
         let num_leds = ((16f64 * (f64::from(v) / f64::from(total))).round()) as u64;
         for _ in 0..num_leds {
-            leds.push(colors[i]);
+            leds.push(colors[i].clone());
         }
     }
 
     fill_column(&mut uhd, col, leds)
+}
+
+fn display_metrics(mut uhd: &mut UnicornHatHd, metrics: Vec<DisplayType>) -> Result<(), Error> {
+    let mut current_column = 0;
+    for metric in metrics {
+        match metric {
+            DisplayType::ColumnRatio { width: width @ _, values: values @ _, colors: colors @ _ } => {
+                for i in 0..width {
+                    fill_column_ratio(&mut uhd, current_column, values.clone(), colors.clone())?;
+                    current_column += 1;
+                }
+            },
+            DisplayType::ColumnCount { width: _, value: _ } => unimplemented!(),
+        }
+    }
+
+    Ok(())
 }
